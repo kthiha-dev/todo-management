0be9d5d2c78a209a1f560e4924571366e30891b7
Kevin Kyaw <kthiha3@gmail.com>
2023-05-06 20:07:50 +0630

    add search and filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,17 +6,22 @@ import {
   Post,
   Delete,
   Patch,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasks(filterDto);
+    }
     return this.tasksService.getAllTasks();
   }
 
