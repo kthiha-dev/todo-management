9a1b40229d17af9370b34c856dccd006334022c2
Kevin Kyaw <kthiha3@gmail.com>
2023-06-02 04:50:25 +0630

    add auth module and hash password

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  maxLength,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(10)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  // @Matches(/((?=.*d)|(?=.*W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  //   message: 'Password too weak',
+  // })
+  password: string;
+}
